87ef5c765819f1a4b6746a623c8ee626ea921722
Olivia Goodman <oliviagoodman@Olivias-Air.hub>
2020-05-13 13:38:01 +1200

    version 3

--- menu driven maths program.py
+++ menu driven maths program.py
@@ -1,17 +1,32 @@
 #menu driven maths
-#version 2
+#version 3
 #olivia g 13/5/20
-#in this version I am doing the rest of the input
+#in this version I am making the program perform the calculations
 
+#constant - operations offered in the menu
 MENU_OPTIONS = [["Addition", "a"], ["Subtraction", "s"], ["Multiplication", "m"], ["Division", "d"], ["Quit", "q"]]
 
+#integer input
 int_one = int(input("Enter the first integer: "))
 int_two = int(input("Enter the second integer: "))
 print(int_one)
 print(int_two)
 
+#choosing a menu option
 for i in MENU_OPTIONS:
     print("{}: press {}.".format(i[0], i[1]),end =" ")
 print()
 opt_choice = input("Choose one of these options: ")
 print(opt_choice)
+
+#performing the operation
+if opt_choice == MENU_OPTIONS[0][1]:
+    print("{} + {} = {}".format(int_one, int_two, int_one + int_two))
+elif opt_choice == MENU_OPTIONS[1][1]:
+    print("{} - {} = {}".format(int_one, int_two, int_one - int_two))
+elif opt_choice == MENU_OPTIONS[2][1]:
+    print("{} * {} = {}".format(int_one, int_two, int_one * int_two))
+elif opt_choice == MENU_OPTIONS[3][1]:
+    print("{} / {} = {:.2f}".format(int_one, int_two, int_one / int_two))
+else:
+    print("Goodbye!")
