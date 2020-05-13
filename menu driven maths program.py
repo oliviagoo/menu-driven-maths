e3dd09bf4981234b6bf1aa88d7d706f7f1aee9ca
Olivia Goodman <oliviagoodman@Olivias-Air.hub>
2020-05-13 12:56:36 +1200

    version 1

---
+++ menu driven maths program.py
@@ -0,0 +1,9 @@
+#menu driven maths
+#version 1
+#olivia g 13/5/20
+#in this version I am getting the basic input to work
+
+int_one = int(input("Enter the first integer: "))
+int_two = int(input("Enter the second integer: "))
+print(int_one)
+print(int_two)
