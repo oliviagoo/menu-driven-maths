b36d5a35e63a516af8779fd8c4f0b9b6e06ef3af
Olivia Goodman <oliviagoodman@Olivias-Air.hub>
2020-05-13 13:25:52 +1200

    version 2

--- menu driven maths program.py
+++ menu driven maths program.py
@@ -1,9 +1,17 @@
 #menu driven maths
-#version 1
+#version 2
 #olivia g 13/5/20
-#in this version I am getting the basic input to work
+#in this version I am doing the rest of the input
+
+MENU_OPTIONS = [["Addition", "a"], ["Subtraction", "s"], ["Multiplication", "m"], ["Division", "d"], ["Quit", "q"]]
 
 int_one = int(input("Enter the first integer: "))
 int_two = int(input("Enter the second integer: "))
 print(int_one)
 print(int_two)
+
+for i in MENU_OPTIONS:
+    print("{}: press {}.".format(i[0], i[1]),end =" ")
+print()
+opt_choice = input("Choose one of these options: ")
+print(opt_choice)
